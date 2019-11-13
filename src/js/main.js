fcd1a6e9119e094bad48bcfbeb62233eb091c058
Ruby Cardenas <rcardenas@bradsdeals.com>
2019-11-12 23:30:41 -0600

    adding year slider feature

--- src/js/main.js
+++ src/js/main.js
@@ -136,3 +136,13 @@ function stopPropagation(evt) {
     evt.cancelBubble = true;
   }
 }
+
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
+
